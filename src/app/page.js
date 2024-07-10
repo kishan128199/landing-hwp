e1e04c93a7a7c94190f73d4756205e845dbb1e37
kishan26028 <kp990450@gmail.com>
2024-07-10 18:49:34 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,234 @@
-import Image from "next/image";
-
+'use client';
+import Image from 'next/image';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from '@mui/material';
+import { FaPlus, FaMinus } from 'react-icons/fa';
+import { useState } from 'react';
 export default function Home() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  console.log(expanded);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <header className="header">
+        <div className="overlay">
+          <p className="banner-description">
+            Keep Smiling as Humanity is still alive
+          </p>
+          <h1 className="banner-title">
+            Empowering Holistic Fitness and Wellness
+          </h1>
         </div>
-      </div>
+        <object
+          style={{
+            margin: '12rem auto auto auto',
+            width: '100%',
+          }}
+          width={100}
+          height={100}
+          type="image/svg+xml"
+          data="/svg/mouse-scroll.svg"
+        ></object>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="services">
+        <h2 className="services-title">Our Services</h2>
+        <p className="services-description">
+          We offer a range of services designed to promote physical well-being,
+          create supportive communities, and provide comprehensive health and
+          wealth protection.
+        </p>
+        <div className="service-group">
+          <div className="service-1">
+            <div className="service-contr">
+              <div>
+                <h3>Yoga and Workout Training</h3>
+                <hr />
+                <p>
+                  Engage Wellness coaches and certified Fitness trainers to
+                  conduct regular online webinars or offline seminars
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="service-2">
+            <div className="service-contr">
+              <div>
+                <h3>Holistic Wellness</h3>
+                <hr />
+                <p>
+                  Encouraging participants to adopt a holistic approach to
+                  physical well-being. Our goal is to instill sustainable health
+                  habits
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="service-3">
+            <div className="service-contr">
+              <div>
+                <h3>Community </h3>
+                <hr />
+                <p>
+                  Foster a collaborative approach to health promotion by
+                  integrating fitness and wellness services through a
+                  combination of educational initiative
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="community">
+        <div className="community-contr">
+          <div className="community-content-contr">
+            <div>
+              <h2>Collective Support</h2>
+              <p
+                style={{
+                  textAlign: 'justify',
+                  width: '90%',
+                  fontSize: '1rem',
+                  fontWeight: '600',
+                  textIndent: '100px',
+                }}
+              >
+                Cultivate a community among our customers through voluntary
+                support mechanisms. Encourage sharing experiences, insights, and
+                assistance among members. Our goal is to create a supportive
+                network where individuals work together towards common health
+                and wealth protection goals.
+              </p>
+            </div>
+            <div className="accordion-contr" style={{ marginTop: '30px' }}>
+              <Accordion
+                expanded={expanded === 'panel1'}
+                onChange={handleChange('panel1')}
+                style={{
+                  background: 'transparent',
+                  boxShadow: 'none',
+                  width: '100%',
+                }}
+              >
+                <AccordionSummary
+                  classes={{ content: 'accordion-title' }}
+                  style={{
+                    width: '80%',
+                    height: '35px',
+                    minHeight: '0px !important',
+                  }}
+                  expandIcon={expanded !== 'panel1' ? <FaPlus /> : <FaMinus />}
+                >
+                  <h4> Shared Experiences</h4>
+                </AccordionSummary>
+                <AccordionDetails
+                  style={{
+                    width: '70%',
+                    textAlign: 'justify',
+                    textIndent: '30px',
+                  }}
+                >
+                  <p style={{ textAlign: 'left' }}>
+                    Encourage customers to share their personal health and
+                    wealth protection journeys, offering valuable insights and
+                    fostering mutual support.
+                  </p>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel2'}
+                onChange={handleChange('panel2')}
+                style={{ background: 'transparent', boxShadow: 'none' }}
+              >
+                <AccordionSummary
+                  classes={{ content: 'accordion-title' }}
+                  style={{
+                    width: '80%',
+                    height: '35px',
+                    minHeight: '0px !important',
+                  }}
+                  expandIcon={expanded !== 'panel2' ? <FaPlus /> : <FaMinus />}
+                >
+                  <h4>Voluntary Support</h4>
+                </AccordionSummary>
+                <AccordionDetails
+                  style={{
+                    width: '70%',
+                    textAlign: 'justify',
+                    textIndent: '30px',
+                  }}
+                >
+                  <p style={{ textAlign: 'left' }}>
+                    Facilitate avenues where customers can provide assistance
+                    and guidance to each other, creating a strong community
+                    network.
+                  </p>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel3'}
+                onChange={handleChange('panel3')}
+                style={{ background: 'transparent', boxShadow: 'none' }}
+              >
+                <AccordionSummary
+                  classes={{ content: 'accordion-title' }}
+                  style={{
+                    width: '80%',
+                    height: '35px',
+                    minHeight: '0px !important',
+                  }}
+                  expandIcon={expanded !== 'panel13' ? <FaPlus /> : <FaMinus />}
+                >
+                  <h4>Collective Goals</h4>
+                </AccordionSummary>
+                <AccordionDetails
+                  style={{
+                    width: '70%',
+                    textAlign: 'justify',
+                    textIndent: '30px',
+                  }}
+                >
+                  <p style={{ textAlign: 'left' }}>
+                    Promote a collaborative environment where individuals work
+                    together to achieve common health and financial objectives,
+                    enhancing the overall effectiveness of our services.
+                  </p>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          </div>
+          <div className="community-img-contr">
+            <Image
+              width={900}
+              src={'/support-community.jpg'}
+              height={500}
+              alt="hand chain"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="contact">
+        <h2>Contact Us</h2>
+        <p>
+          For any query or question regarding our services, please feel free to
+          contact us.
+        </p>
+        <p>Mobile No.: 9558696217</p>
+        <p>Email: keepsmiling@aimhwpservices.com</p>
+        <p>Email: aimhwpservices@gmail.com</p>
+        <p>Application on Google Play store: AIM HWP</p>
+      </section>
+      <footer className="footer">
+        <p>&copy; 2024 AIM HWP SERVICES LLP. All rights reserved.</p>
+      </footer>
     </main>
   );
 }
